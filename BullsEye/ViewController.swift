efa0fd0421ac33ce0e45adf070930243e99e3dae
Vetrichelvan <vetri02@gmail.com>
2019-05-06 23:21:34 +0530

    Slider and the rest

--- BullsEye/ViewController.swift
+++ BullsEye/ViewController.swift
@@ -9,24 +9,48 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    var currentValue: Int = 0
+    var targetValue: Int = 0
+    
+    @IBOutlet weak var slider: UISlider!
+    @IBOutlet weak var targetLabel: UILabel!
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        let roundedValue = slider.value.rounded()
+        currentValue = Int(roundedValue)
+        startNewRound()
     }
 
     @IBAction func showAlert() {
-        let alert = UIAlertController(title: "Hello World", message: "This is my first app", preferredStyle: .alert)
+        let message = "The value of slider is now:\(currentValue)" +
+        "\n target value: \(targetValue)"
         
-        let action = UIAlertAction(title: "Awesome", style: .default, handler: nil)
+        let alert = UIAlertController(title: "Hello World", message: message, preferredStyle: .alert)
+        
+        let action = UIAlertAction(title: "Ok", style: .default, handler: nil)
         
         alert.addAction(action)
         
         present(alert, animated: true, completion: nil)
+        startNewRound()
     }
     
     @IBAction func sliderMoved(_ slider:UISlider) {
-        print("Slider Value \(slider.value)")
+        let roundedValue = slider.value.rounded()
+        currentValue = Int(roundedValue)
+    }
+    
+    func startNewRound() {
+        targetValue = Int.random(in: 1...100)
+        currentValue = 50
+        slider.value = Float(currentValue)
+        updateLabel()
+    }
+    
+    func updateLabel() {
+        targetLabel.text = String(targetValue)
     }
     
    
